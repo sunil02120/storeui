9f5a7f999ba06ba3e5c71d74c11a9e95e0b3f74c
sunil <sunil02120@gmail.com>
2023-09-28 13:21:53 +0530

    updated profit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 import Navbar from './module/Navbar';
 import TextForm from './module/TextArea';
 import HighestSellingItem from './module/HighestSellingItem';
+import HighestProfitItem from './module/HighestProfitItem';
 
 function App() {
   return (
     <>
-    <Navbar title={'Store Item Analyzer'}/>
-    <HighestSellingItem heading="Highest Selling Item"></HighestSellingItem>    
+      <Navbar title={"Store Item Analyzer"} />
+      <table>
+        <tr>
+          <td><HighestSellingItem heading="Highest Selling Item"></HighestSellingItem></td>
+          <td><HighestProfitItem heading="Highest Profit Item"></HighestProfitItem></td>
+        </tr>
+      </table>
     </>
   );
 }
